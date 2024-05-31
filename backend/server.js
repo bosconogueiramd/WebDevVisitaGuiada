aa28757655bbf08a00987ae56f677d6b184261fa
Joao <148392619+bosconogueiramd@users.noreply.github.com>
2024-05-31 17:26:23 -0300

    Resolve merge conflict in server.js

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-<<<<<<< HEAD
 const registerUserRouter = require('./routes/userRoutes');
 const registerStepRouter = require('./routes/stepRoutes');
-=======
-const userRoutes = require('./routes/userRoutes');
-const stepRoutes = require('./routes/stepRoutes');
-const authenticateToken = require('./middleware/auth');
->>>>>>> e1c2a941 (Atualizações e melhorias no projeto visita-guiada)
 
 const app = express();
 const PORT = 5001;
@@ -24,16 +18,11 @@ mongoose.connect('mongodb+srv://bosconogueira01:teste1234@joaobosco.dxw25lr.mong
 .then(() => console.log('Conectado ao MongoDB'))
 .catch((error) => console.error('Erro ao conectar ao MongoDB:', error));
 
-<<<<<<< HEAD
 router = express.Router();
 registerUserRouter(router);
 registerStepRouter(router);
 
 app.use("/api", router);
-=======
-app.use('/api', authenticateToken, userRoutes);
-app.use('/api', authenticateToken, stepRoutes);
->>>>>>> e1c2a941 (Atualizações e melhorias no projeto visita-guiada)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
